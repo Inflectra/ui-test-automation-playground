a9803334d99a58d462857637eedfd99e98d105ab
Carolina Campos <carolinacampos.dev@gmail.com>
2025-05-04 15:37:35 -0300

    test: identifica botão e confirma ação no alerta

--- cypress/e2e/ui-test.cy.js
+++ cypress/e2e/ui-test.cy.js
@@ -3,8 +3,16 @@ describe('UI Test Automation Playground', () => {
         cy.visit('/dynamicid')
 
         cy.contains('button', 'Button with Dynamic ID')
-          .should('be.visible')
-          .click()
-          .should('have.text', 'Button with Dynamic ID')
+            .should('be.visible')
+            .click()
+            .should('have.text', 'Button with Dynamic ID')
+    })
+
+    it.only('Deve identificar o botão primário com a classe btn-primary e confirmar o alerta', () => {
+        cy.visit('/classattr')
+
+        cy.on('window:alert', cy.stub().as('alert'))
+        cy.get('.btn-primary').click()
+        cy.get('@alert').should('have.been.calledWith', 'Primary button pressed')
     })
 })
